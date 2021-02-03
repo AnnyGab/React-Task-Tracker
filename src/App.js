327fb94bda49b9dc22f17b0a8964700e7d9bf1a7
AnnyGab <AnnyGab@users.noreply.github.com>
2021-02-02 21:35:41 -0600

    Task component created

--- src/App.js
+++ src/App.js
@@ -25,12 +25,21 @@ const App = () => {
     }
 ])
 
+//Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==
+   id))
+}
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks = {tasks}/>
+      <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTask} 
+      />
     </div>
   )
 }
 
-export default App;
+export default App
