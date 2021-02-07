aaf56dc8883676b19ebd77ae4cc9563b2ce117b3
AnnyGab <AnnyGab@users.noreply.github.com>
2021-02-07 15:47:56 -0600

    UI Green vertical line when tasks have a reminder

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ const App = () => {
         id: 1,
         text: 'Doctors Appoinment',
         day: 'Feb 5th at 2:30pm',
-        remider: true,
+        reminder: true,
     },
     {
         id: 2,
         text: 'School meeting',
         day: 'Feb 6th at 1:30pm',
-        remider: true,
+        reminder: true,
     },
     {
         id: 3,
         text: 'Food Shopping',
         day: 'Feb 5th at 2:30pm',
-        remider: false,
+        reminder: false,
     }
 ])
 
@@ -31,12 +31,22 @@ const deleteTask = (id) => {
    id))
 }
 
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+    task.id === id ? { ...task, reminder: 
+    !task.reminder } : task
+    )
+    )
+}
+
   return (
     <div className='container'>
       <Header />
       {tasks.length > 0 ? <Tasks 
         tasks={tasks} 
-        onDelete={deleteTask}/> : 'No Tasks To Show'}
+        onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   )
 }
